b60cb3f28887628c64a0022238f596859da17b3d
MSiesto <miguelsiesto@gmail.com>
2023-01-14 16:38:38 +0100

    Updating CSS for it to match BEM

--- src/Footer/Footer.Component.js
+++ src/Footer/Footer.Component.js
@@ -13,119 +13,131 @@ import instagramIcon from "../Logos/instagram__logo.png";
 
 function FooterCard({ className }) {
   return (
-    <div className={clsx(className, "container__footer")}>
-      <div className="container__columns--footer">
-        <div className="column">
+    <div className={clsx(className, "Footer")}>
+      <div className="Footer__ColumnsContainer">
+        <div className="Footer__Column">
           <p>Asistencia</p>
-          <Button className="container__button--column">Centro de ayuda</Button>
-          <Button className="container__button--column">Aircover</Button>
-          <Button className="container__button--column">
+          <Button className="Footer__Button--ColumnButton">
+            Centro de ayuda
+          </Button>
+          <Button className="Footer__Button--ColumnButton">Aircover</Button>
+          <Button className="Footer__Button--ColumnButton">
             Información de seguridad
           </Button>
-          <Button className="container__button--column">
+          <Button className="Footer__Button--ColumnButton">
             Accesibilidad para todos
           </Button>
-          <Button className="container__button--column">
+          <Button className="Footer__Button--ColumnButton">
             Opciones de cancelación
           </Button>
-          <Button className="container__button--column">
+          <Button className="Footer__Button--ColumnButton">
             Nuestra respuesta ante la COVID-19
           </Button>
-          <Button className="container__button--column">
+          <Button className="Footer__Button--ColumnButton">
             ¿Problemas en tu barrio?
           </Button>
         </div>
-        <div className="column">
+        <div className="Footer__Column">
           <p>Comunidad</p>
-          <Button className="container__button--column">
+          <Button className="Footer__Button--ColumnButton">
             Airbnb.org para emergencias
           </Button>
-          <Button className="container__button--column">
+          <Button className="Footer__Button--ColumnButton">
             Ayuda a los refugiados afganos
           </Button>
-          <Button className="container__button--column">
+          <Button className="Footer__Button--ColumnButton">
             Lucha contra la discriminación
           </Button>
         </div>
-        <div className="column">
+        <div className="Footer__Column">
           <p>Cómo ser anfitrión</p>
-          <Button className="container__button--column">Hazte anfitrión</Button>
-          <Button className="container__button--column">
+          <Button className="Footer__Button--ColumnButton">
+            Hazte anfitrión
+          </Button>
+          <Button className="Footer__Button--ColumnButton">
             AirCover para anfitriones
           </Button>
-          <Button className="container__button--column">
+          <Button className="Footer__Button--ColumnButton">
             Recursos para anfitriones
           </Button>
-          <Button className="container__button--column">
+          <Button className="Footer__Button--ColumnButton">
             Visita el foro de la comunidad
           </Button>
-          <Button className="container__button--column">
+          <Button className="Footer__Button--ColumnButton">
             Ser un anfitrión responsable
           </Button>
         </div>
-        <div className="column">
+        <div className="Footer__Column">
           <p>Acerca de</p>
-          <Button className="container__button--column">Newsroom</Button>
-          <Button className="container__button--column">
+          <Button className="Footer__Button--ColumnButton">Newsroom</Button>
+          <Button className="Footer__Button--ColumnButton">
             Descubre las nuevas funciones
           </Button>
-          <Button className="container__button--column">
+          <Button className="Footer__Button--ColumnButton">
             Carta de los fundadores
           </Button>
-          <Button className="container__button--column">Empleo</Button>
-          <Button className="container__button--column">Inversores</Button>
+          <Button className="Footer__Button--ColumnButton">Empleo</Button>
+          <Button className="Footer__Button--ColumnButton">Inversores</Button>
         </div>
       </div>
 
-      <div className="container__info--footer">
-        <div className="container__info--footerLeft">
-          <img className="copyrightIcon" src={copyrightIcon} alt="img" />
-          <p className="AirbnbInc">2022 Airbnb, Inc.</p>
-          <img className="dot" src={dot} alt="dot" />
-          <Button className="container__button--infoFooter">Privacidad</Button>
-          <img className="dot" src={dot} alt="dot" />
-          <Button className="container__button--infoFooter">Condiciones</Button>
-          <img className="dot" src={dot} alt="dot" />
-          <Button className="container__button--infoFooter">
+      <div className="Footer__BottomStrap">
+        <div className="Footer__BottomStrap-Left">
+          <img
+            className="Footer__copyrightIcon"
+            src={copyrightIcon}
+            alt="img"
+          />
+          <p className="Footer__AirbnbInc">2022 Airbnb, Inc.</p>
+          <img className="Footer__dot" src={dot} alt="dot" />
+          <Button className="Footer__Button--BottomStrap">Privacidad</Button>
+          <img className="Footer__dot" src={dot} alt="dot" />
+          <Button className="Footer__Button--BottomStrap">Condiciones</Button>
+          <img className="Footer__dot" src={dot} alt="dot" />
+          <Button className="Footer__Button--BottomStrap">
             Mapa del sitio
           </Button>
-          <img className="dot" src={dot} alt="dot" />
-          <Button className="container__button--infoFooter">
+          <img className="Footer__dot" src={dot} alt="dot" />
+          <Button className="Footer__Button--BottomStrap">
             Datos de la empresa
           </Button>
         </div>
-        <div className="container__info--footerRigth">
-          <div className="footerRigth__container--Espanol">
-            <button className="button__footerWorld">
-              <img
-                className="footerWorldIcon"
-                src={footerWorldIcon}
-                alt="img"
-              ></img>
-              <p className="spanishText">Español(ES)</p>
-            </button>
-          </div>
-          <div className="footerRigth__container--Euro">
-            <button className="euroButton">
-              <img className="euroIcon" src={euroIcon} alt="img"></img>
-              <p className="euroText"> EUR </p>
-            </button>
-          </div>
-          <div className="footerRigth__container--Redes">
-            <button className="facebookButton">
-              <img className="facebookIcon" src={facebookIcon} alt="img"></img>
-            </button>
-            <button className="twitterButton">
-              <img className="twitterIcon" src={twitterIcon} alt="img"></img>
-            </button>
-            <button className="instagramButton">
-              <img
-                className="instagramIcon"
-                src={instagramIcon}
-                alt="img"
-              ></img>
-            </button>
-          </div>
+        <div className="Footer__BottomStrap-Rigth">
+          <button className="Footer__WorldEspañolButton">
+            <img
+              className="Footer__WorldIconImg"
+              src={footerWorldIcon}
+              alt="img"
+            ></img>
+            <p className="Footer__SpanishText">Español(ES)</p>
+          </button>
+
+          <button className="Footer__euroButton">
+            <img className="Footer__euroIcon" src={euroIcon} alt="img"></img>
+            <p className="Footer__euroText"> EUR </p>
+          </button>
+
+          <button className="Footer__facebookButton">
+            <img
+              className="Footer__facebookImg"
+              src={facebookIcon}
+              alt="img"
+            ></img>
+          </button>
+          <button className="Footer__twitterButton">
+            <img
+              className="Footer__twitterImg"
+              src={twitterIcon}
+              alt="img"
+            ></img>
+          </button>
+          <button className="Footer__instagramButton">
+            <img
+              className="Footer__instagramImg"
+              src={instagramIcon}
+              alt="img"
+            ></img>
+          </button>
         </div>
       </div>
     </div>
